13e38a7a9fd33012a322b690ec6bdfd21ce2ff40
JLC2141 <chodkowskij@michigan.gov>
2026-01-31 15:09:24 -0500

    Updated script

--- analysis_script.sh
+++ analysis_script.sh
@@ -2,17 +2,22 @@
 mkdir fastqc_raw
 fastqc *.fastq.gz -o fastqc_raw/
 #Run multiqc in the fastqc_raw directory
-#multiQC fastqc_raw/ -o fastqc_raw/multiqc_report/
+multiqc fastqc_raw/ -o fastqc_raw/
 
 #Run trimming
+mkdir fastp_results
 fastp -i SRR3747659_1.fastq.gz \
 -I SRR3747659_2.fastq.gz \
 -o SRR3747659_1_trimmed.fastq.gz \
--O SRR3747659_2_trimmed.fastq.gz
+-O SRR3747659_2_trimmed.fastq.gz \
+--html fastp_results/fastp_report.html \
+--json fastp_results/fastp_report.json
 
 #Run FASTQC on trimmed reads
 mkdir fastqc_trimmed
 fastqc *_trimmed.fastq.gz -o fastqc_trimmed/
+#Run multiqc in the fastqc_trimmed directory
+multiqc fastqc_trimmed/ -o fastqc_trimmed/
 
 #Run assembly with SPAdes
 spades.py -1 SRR3747659_1_trimmed.fastq.gz \
